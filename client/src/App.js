82f166a6eb5985f5935755d4c46a2b1e6f1f6621
xhevo <xhevat.ibraimi@gmail.com>
2019-11-01 16:48:29 +0100

    fixed errors in react

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
-const search = () => {
-
-};
 
 const App = () => {
 
@@ -15,8 +10,8 @@ const App = () => {
           <h1>Weather \_(0_o)_/</h1>
         </div>
         <div className="col-md-8 offset-md-2">
-          <div className="input-gro$p mb-3">
-            <input type="text" className="form-control" placeholder="Type City Name" onClick={this.search()} />
+          <div className="input-group mb-3">
+            <input type="text" className="form-control" placeholder="Type City Name" />
             <div className="input-group-append">
               <button className="btn btn-outline-secondary" type="button">
                 <i className="fa fa-search"></i>
